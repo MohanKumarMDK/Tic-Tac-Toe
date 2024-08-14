ca96647a033184b937a0308119522e62753969e8
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-08-14 21:03:23 +0530

    styling changes

--- src/App.js
+++ src/App.js
@@ -11,15 +11,17 @@ const App = () => {
     if (currentWinner) {
       setWinner(currentWinner);
       alert(`Winner: ${currentWinner}`);
+      setTimeout(resetGame, 2000); // Automatically restart the game after 2 seconds
     } else if (board.every(square => square !== null)) {
       setWinner('Tie');
       alert('The game is a tie!');
+      setTimeout(resetGame, 2000); // Automatically restart the game after 2 seconds
     }
-  }, [board]); 
+  }, [board]);
 
   const handleClick = (index) => {
     const newBoard = board.slice();
-    if (winner || newBoard[index]) return; 
+    if (winner || newBoard[index]) return;
     newBoard[index] = isXNext ? 'X' : 'O';
     setBoard(newBoard);
     setIsXNext(!isXNext);
@@ -48,7 +50,7 @@ const App = () => {
   };
 
   return (
-    <div className="font-black">
+    <div className="flex flex-col justify-center align-middle m-5 p-5 text-4xl text-center text-white">
       <h1>Tic Tac Toe</h1>
       <Board squares={board} onClick={handleClick} />
       <p>{winner ? `Result: ${winner}` : `Next player: ${isXNext ? 'X' : 'O'}`}</p>
@@ -58,7 +60,7 @@ const App = () => {
 };
 
 const Board = ({ squares, onClick }) => (
-  <div className="row-cols-4 p-5">
+  <div className="grid grid-rows-3 grid-flow-col p-5 align-middle justify-center">
     {squares.map((square, index) => (
       <Square key={index} value={square} onClick={() => onClick(index)} />
     ))}
@@ -66,7 +68,7 @@ const Board = ({ squares, onClick }) => (
 );
 
 const Square = ({ value, onClick }) => (
-  <button className="btn bg-secondary-subtle btn-lg p-5 m-2" onClick={onClick}>
+  <button type='button' className="text-white font-extrabold text-5xl bg-blue-300 hover:shadow-[0_0_15px_5px_rgba(100,116,256,1)] focus:ring-4 focus:ring-blue-300 rounded-lg px-5 py-2.5 me-2 mb-2 focus:outline-none w-28 h-28 " onClick={onClick}>
     {value}
   </button>
 );
